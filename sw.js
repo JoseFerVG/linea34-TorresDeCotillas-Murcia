d87ef1611f5c228409b67ddf5deeba32768a6631
JoseFerVG <josef.caniles.murcia@gmail.com>
2026-08-22 23:13:14 +0200

    feat: set installed PWA app name to Línea 34

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Ultra-stable cache engine with Network-First strategy
  */
 
-const CACHE_NAME = 'movibus-l34-v10';
+const CACHE_NAME = 'movibus-l34-v11';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
